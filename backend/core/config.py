2e7e1acdcb0643d437d690559f281480fbf6d32b
isathish <isathish@icloud.com>
2025-12-05 18:57:44 +0530

    refactor: update project name from "Advanced CRUD System" to "PYCRUD" in configuration

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Advanced CRUD System"
+    PROJECT_NAME: str = "PYCRUD"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     
